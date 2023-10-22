5c1644553967dc94ea92e37f216a458801f09225
Norbert Nemeth <nth.norbert@gmail.com>
2023-10-22 14:31:21 +0200

    backend.php, new redirect/lang system

--- php/backend.php
+++ php/backend.php
@@ -3,6 +3,7 @@ $siteINFO = new stdClass();
 $siteJSON = loadJSON('json/site.json');
 $siteINFO -> langAvailable = $siteJSON["languages"]["site"];
 $siteINFO -> langUser = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2) : false;
+$siteINFO -> redirect = false;
 
 // Test Server?
 if ($_SERVER['SERVER_NAME'] === 'localhost') {
@@ -25,28 +26,33 @@ $parts = explode("/", $parts[0]);
 if (in_array($parts[1], $siteINFO -> langAvailable)) {
     $siteINFO -> langSite = $parts[1];
     $siteINFO -> page = $parts[2];
-    $siteINFO -> urlIso = true;
 } else {
     $siteINFO -> langSite = in_array($siteINFO -> langUser, $siteINFO -> langAvailable) ? $siteINFO -> langUser : "en";
     $siteINFO -> page = count($parts) === 3 ? $parts[2] : $parts[1];
-    $siteINFO -> urlIso = false;
+    $siteINFO -> redirect = true;
 }
+
 $siteINFO -> page = $siteINFO -> page === "" ? "home" : $siteINFO -> page;
 $langJSON = loadJSON('json/languages/'.$siteINFO -> langSite.'.json');
 
 // 404 Error
 if (!isset($langJSON["nav"][$siteINFO->page])) {
     errorExport("Error 404", $requestURI);
-    header('Location: '.$siteINFO -> mainPath ."404");
+    $siteINFO -> page = "404";
 }
 
-$siteINFO -> link = $siteINFO -> urlIso ? $siteINFO -> mainPath.$siteINFO -> langSite . '/' : $siteINFO -> mainPath;
+$siteINFO -> link = $siteINFO -> mainPath . $siteINFO -> langSite . '/';
+
+// Redirect it? If yes go other page
+if ($siteINFO -> redirect) {
+    urlRedirect($siteINFO);
+}
 
 /*
 echo '<pre>';
-    print_r($parts);
-    print_r($siteINFO);
-    echo '----------';
+print_r($parts);
+print_r($siteINFO);
+echo '----------';
 echo '</pre>';
 */
 ?>
